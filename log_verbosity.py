19e6916a7cf36ffbe91d94a2cc70eed9dce7b2f8
Justin Payne <justin.payne@fda.hhs.gov>
2019-10-17 13:18:45 -0400

    actually runs now

--- log_verbosity.py
+++ log_verbosity.py
@@ -1,9 +1,12 @@
 import argparse
 import logging
+import sys
 
 """
 I often want to control logging options with a repeated -v[vv, vvv] flag; this works
 and I can never remember how I did it.
+
+The use of zip and range ensures a maximum log level no matter how many 'v's you provide.
 
 """
 
@@ -12,11 +15,13 @@ parse = argparse.ArgumentParser()
 parse.add_argument('--verbose', '-v', action='count', dest='log_v', default=0)
 args = parse.parse_args()
 
-for level, _ in zip((logging.ERROR, logging.INFO, logging.DEBUG), range(args.log_v + 1)):
+for level, _ in zip((logging.CRITICAL, logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG), range(args.log_v + 1)):
 	pass
 
 logging.basicConfig(
 	level = level,
 	stream=sys.stderr
 )
+
+print("log level", level)
 
